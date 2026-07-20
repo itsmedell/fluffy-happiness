62d9919a5e95ee5c5bb89a66108e2d924954a587
Dellas <dellasdp403@gmail.com>
2026-07-20 19:27:07 +0700

    Add reusable custom button widget

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+
+  const CustomButton({super.key, required this.text, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: FilledButton(
+        onPressed: onPressed,
+        child: Text(text),
+      ),
+    );
+  }
+}
